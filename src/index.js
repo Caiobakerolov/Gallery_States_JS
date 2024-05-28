1b1c4c72a29c3eb0d5ae240094c43c40c208be94
Caio Bakerolov <caiobakerolov@icloud.com>
2024-05-28 21:39:29 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//  Arquivo SASS
+import './scss/index.scss'
+
+//dependencias
+import 'jquery'
+import 'bootstrap'
+
+// meus arquivos js
+import './js/core/includes.js'
+import './js/core/plugins/cityButtons.js'
